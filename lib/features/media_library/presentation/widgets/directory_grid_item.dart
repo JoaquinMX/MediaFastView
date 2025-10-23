d1c767a06eb4381e007e74408df18992fc8e0e72
Joaquin Beltran <67206790+JoaquinMX@users.noreply.github.com>
2025-10-23 09:51:36 -0600

    Fix directory thumbnail loading from filesystem

--- lib/features/media_library/presentation/widgets/directory_grid_item.dart
+++ lib/features/media_library/presentation/widgets/directory_grid_item.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import '../../../../core/constants/ui_constants.dart';
@@ -132,8 +134,8 @@ class _DirectoryGridItemState extends State<DirectoryGridItem>
                                 color:
                                     Theme.of(context).colorScheme.surfaceContainerHighest,
                                 child: widget.directory.thumbnailPath != null
-                                    ? Image.asset(
-                                        widget.directory.thumbnailPath!,
+                                    ? Image.file(
+                                        File(widget.directory.thumbnailPath!),
                                         fit: BoxFit.cover,
                                         errorBuilder: (context, error, stackTrace) =>
                                             _buildPlaceholder(),
