60ecd8c6f40c997cce70799ead567ec4dd9f4c28
Joaquin Beltran <67206790+JoaquinMX@users.noreply.github.com>
2025-11-26 00:27:27 -0600

    Enable tag toggling for media selection

--- lib/features/tagging/presentation/widgets/tag_filter_chips.dart
+++ lib/features/tagging/presentation/widgets/tag_filter_chips.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../domain/entities/tag_entity.dart';
 import '../states/tag_state.dart';
 import '../view_models/tag_management_view_model.dart';
 import 'selectable_tag_chip_strip.dart';
@@ -13,12 +14,14 @@ class TagFilterChips extends ConsumerWidget {
     super.key,
     required this.selectedTagIds,
     required this.onSelectionChanged,
+    this.onTagTapped,
     this.maxChipsToShow,
     this.showAllButton = true,
   });
 
   final List<String> selectedTagIds;
   final ValueChanged<List<String>> onSelectionChanged;
+  final Future<void> Function(TagEntity tag, bool isSelected)? onTagTapped;
   final int? maxChipsToShow;
   final bool showAllButton;
 
@@ -31,8 +34,9 @@ class TagFilterChips extends ConsumerWidget {
           tags: tags,
           selectedTagIds: selectedTagIds,
           onSelectionChanged: onSelectionChanged,
+          onTagTapped: onTagTapped,
           maxChipsToShow: maxChipsToShow,
-          showAllChip: showAllButton,
+          showAllChip: showAllButton && onTagTapped == null,
           onOverflowPressed: _shouldShowOverflow(tags.length)
               ? () => _showAllTagsDialog(context)
               : null,
