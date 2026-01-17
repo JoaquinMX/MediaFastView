4f8fc834f29caf31827b3394c63119bb3e1c45e9
Joaquin Beltran <67206790+JoaquinMX@users.noreply.github.com>
2026-01-17 14:38:57 -0800

    Enable iOS security-scoped bookmark access

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,20 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private let bookmarkHandler = BookmarkHandler()
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if let controller = window?.rootViewController as? FlutterViewController {
+      let bookmarkChannel = FlutterMethodChannel(
+        name: "com.joaquinmx.media_fast_view/bookmarks",
+        binaryMessenger: controller.binaryMessenger
+      )
+      bookmarkChannel.setMethodCallHandler(bookmarkHandler.handle)
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
